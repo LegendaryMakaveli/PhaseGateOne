008155fbf764ed1c39411b8e1192dd2cbff33f58
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-08-18 15:44:45 +0100

    task 15 done

---
+++ Java/TaskFiveteen.java
@@ -0,0 +1,12 @@
+public class TaskEleven{
+
+public static void main(String[] args) {
+
+int total = 0;
+for (int count = 1; count <= 100; count++) {
+		total += count;
+	}
+
+System.out.print("The sum of the first 100 natural number is:  " + total);
+}
+}
