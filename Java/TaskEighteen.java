d7f815a1fd48ee9f851224df2d30e890f18c0fd4
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-08-18 16:02:21 +0100

    done

--- Java/TaskEighteen.java
+++ Java/TaskEighteen.java
@@ -5,17 +5,15 @@ public static void main(String[] args) {
 int total = 0;
 int count = 1;
 for (count = 1; count <= 100; count++) {
-	System.out.print(count);
+	if (count % 3 == 0) {
+	System.out.println(count + " " + "Frizz");
+	} else if (count % 5 == 0) {
+		System.out.println(count + " " + "Buzz");
+	} else if (count % 3 == 0 && count % 5 == 0) {
+		System.out.println( count + " " + "FrizzBuzz");
 	}
 
-if (count % 3 == 0) {
-		System.out.print("Frizz");
-		}
-	if (count % 5 == 0) {
-		System.out.print("Buzz");
-		}
-	if (count % 3 == 0 && count % 5 == 0) {
-		System.out.print("FrizzBuzz");
-		}
+	}
+
 }
 }
