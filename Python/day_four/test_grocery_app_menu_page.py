3d326e580558d3fc0fbf607ae925f467b45fbde3
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-08-21 22:44:34 +0100

    test done

--- Python/day_four/test_grocery_app_menu_page.py
+++ Python/day_four/test_grocery_app_menu_page.py
@@ -5,17 +5,32 @@ import grocery_app_menu_page
 
 class TestTheFunctionThatAddItemToList(TestCase) :
 	def test_that_the_function_only_accept_String(makaveli) :
-		makaveli.assertEqual(grocery_app_menu_page.add_item_to_list(["coke"]), "Item Added Successfully")
+		makaveli.assertEqual(grocery_app_menu_page.add_item_to_list("coke"), "Item Added Successfully")
 
 	def test_that_the_function_dont_accept_integers(makaveli) :
-		makaveli.assertRaises(ValueError, grocery_app_menu_page.add_item_to_list, ([87,99]))
+		makaveli.assertRaises(ValueError, grocery_app_menu_page.add_item_to_list, (89))
 
 	def test_that_the_function_dont_accept_float(makaveli) :
-		makaveli.assertRaises(ValueError, grocery_app_menu_page.add_item_to_list, ([87.8,99.99,2.8]))
+		makaveli.assertRaises(ValueError, grocery_app_menu_page.add_item_to_list, (89.89))
+
+	def test_the_code_functionality(makaveli) :
+		grocery_app_menu_page.empty_list.clear()
+		grocery_app_menu_page.empty_list.append("coke")
+		makaveli.assertIn("coke",grocery_app_menu_page.empty_list)
+		
 
 
 class TestTheFunctionThatRemoveItemFromTheList(TestCase) :
-	def test_that_the_function_exist(makaveli) :
-		 grocery_app_menu_page.remove_items(["coke"])
+	def test_that_function_dont_accept_float(makaveli) :
+		makaveli.assertRaises(ValueError, grocery_app_menu_page.remove_items,(78.89))
+	
+	def test_that_function_dont_accept_int(makaveli) :
+		makaveli.assertRaises(ValueError, grocery_app_menu_page.remove_items, (78))
 
-	def 
+
+
+
+
+
+
+
