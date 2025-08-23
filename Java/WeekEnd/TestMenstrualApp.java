c6f36977a89891a0bf8c8bd0441a5e94f7ed76e7
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-08-23 22:06:12 +0100

    i have been commiting since morning sir but i dont know i suppose push to make it reflex on my repo

---
+++ Java/WeekEnd/TestMenstrualApp.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+ import static org.junit.jupiter.api.Assertions.*;
+
+
+class TestMentrualCalculatFlow{
+	@Test
+		public void calculateMenstrualFlow(){
+			MenstualApp newMenstualApp = new MenstualApp();
+		}
+
+}
+
+
+
+class TestOvulationPeriodCalculation{
+	@Test
+		public void getOvulationPeriod(){
+			MenstualApp newMenstualApp = new MenstualApp();
+		}
+
+}
+
+
+
+class TestSafeDaysCalculation{
+	@Test
+		public void calculateSafeDays(){
+			MenstualApp newMenstualApp = new MenstualApp();
+		}
+
+}
+
+
+class TestFunctionThatConvertMonthNameToNumber{
+	@Test
+		public void getMonthNumber(){
+			MenstualApp newMenstualApp = new MenstualApp();
+		}
+
+}
+
